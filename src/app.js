33aab8055beee3cc8935a437622fee405cf945fc
chetnagoli <2320030372cseh@gmail.com>
2026-07-02 19:56:28 +0530

    Update error handler

--- src/app.js
+++ src/app.js
@@ -1,19 +1,21 @@
 const express = require("express");
 
 const eventRoutes = require("./routes/eventRoutes");
+const errorHandler = require("./middleware/errorHandler");
 
 const app = express();
 
 app.use(express.json());
 
-// Health check
 app.get("/", (req, res) => {
     res.json({
         message: "Event Dispatcher API Running"
     });
 });
 
-// Assignment API
 app.use("/api/v1", eventRoutes);
+
+// Keep this LAST
+app.use(errorHandler);
 
 module.exports = app;
